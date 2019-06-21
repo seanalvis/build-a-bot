010252e62fcaf3da6335c99229cd912b0b6c4035
Sean Walker <swalker@e-ins.net>
2019-06-21 17:16:38 -0400

    Adding new files and updating repo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <main>
+            <RobotBuilder />
+        </main>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    // import HomePage from "./home/HomePage.vue";
+    import RobotBuilder from "./build/RobotBuilder.vue";
+    export default {
+        name: "app",
+        components: {
+            RobotBuilder
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    body {
+        background: linear-gradient(to bottom, #555, #999);
+        background-attachment: fixed;
+    }
+
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+    }
+
+    main {
+        margin: 0 auto;
+        padding: 30px;
+        background-color: #fff;
+        width: 1024px;
+        min-height: 300px;
+    }
+
 </style>
